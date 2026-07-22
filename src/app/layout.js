d7ea068b62aa2a1561de7e21519e415b9a8c38b5
Xyron Uy <xyronuysoftware@gmail.com>
2026-07-22 20:55:29 +0800

    V1: added favicons for the website

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,9 +68,11 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       className={`${bebas.variable} ${montserrat.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Home Sports Bar" />
+      </head>
       <body className="min-h-full flex flex-col">
         <LocalBusinessJsonLd />
         <IntroSplash />
